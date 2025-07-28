08c459585c1e32be72d08b482d46793a7033da03
agertul <agertul@gmail.com>
2025-07-28 11:21:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-
 function setLanguage(lang) {
   document.querySelectorAll('[data-tr]').forEach(el => {
-    el.innerText = lang === 'en' ? el.getAttribute('data-en') : el.getAttribute('data-tr');
+    if (lang === 'en') {
+      el.innerText = el.getAttribute('data-en');
+    } else if (lang === 'ar') {
+      el.innerText = el.getAttribute('data-ar');
+    } else {
+      el.innerText = el.getAttribute('data-tr');
+    }
   });
+
+  // Sayfa yönünü Arapça için sağdan sola yapalım
+  if (lang === 'ar') {
+    document.body.dir = 'rtl';
+  } else {
+    document.body.dir = 'ltr';
+  }
+
+  // HTML lang değiştir
+  document.documentElement.lang = lang;
 }
+
+// Varsayılan dil Türkçe
 window.onload = () => setLanguage('tr');
